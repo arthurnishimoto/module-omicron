46dcdcf0c031e36eae80e990268f655e55f17280
Arthur Nishimoto <arthur.nishimoto@gmail.com>
2016-09-02 19:58:42 -0500

    NEW: Added cubemap screenshot generator for CAVE2 skybox controller

--- Scripts/CAVE2SkyboxController.cs
+++ Scripts/CAVE2SkyboxController.cs
@@ -6,8 +6,12 @@ public class CAVE2SkyboxController : MonoBehaviour {
     public LayerMask skyboxCullingMask;
     LayerMask lastCullingMask;
 
-	// Use this for initialization
-	void Start () {
+    public bool takeAScreenShot;
+    public string screenshotPath = "CAVE2SkyboxScreenShots";
+    public string screenshotLabel = "";
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
@@ -22,5 +26,40 @@ public class CAVE2SkyboxController : MonoBehaviour {
             }
             lastCullingMask = skyboxCullingMask;
         }
+
+        if( takeAScreenShot )
+        {
+            CubeMapScreenShot();
+            takeAScreenShot = false;
+        }
 	}
+
+    void CubeMapScreenShot()
+    {
+        Camera[] cameras = GetComponentsInChildren<Camera>();
+
+        foreach(Camera c in cameras)
+        {
+            RenderTexture rc = c.targetTexture;
+
+            RenderTexture tempRT = new RenderTexture(rc.width, rc.height, 24);
+            c.targetTexture = tempRT;
+            c.Render();
+
+            RenderTexture.active = tempRT;
+
+            Texture2D tx = new Texture2D(rc.width, rc.height, TextureFormat.RGB24, false);
+            // false, meaning no need for mipmaps
+            tx.ReadPixels(new Rect(0, 0, rc.width, rc.height), 0, 0);
+
+            RenderTexture.active = null; //can help avoid errors 
+            c.targetTexture = rc;
+            Destroy(tempRT);
+
+            byte[] bytes;
+            bytes = tx.EncodeToPNG();
+
+            System.IO.File.WriteAllBytes(Application.dataPath+"/"+ screenshotPath + "/"+ screenshotLabel +"-"+c.name+".png", bytes);
+        }
+    }
 }
