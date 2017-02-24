41fd82a7b855234b5e21fa506cd75b2d2e2881d9
Arthur Nishimoto <arthur.nishimoto@gmail.com>
2017-02-24 11:44:04 -0600

    NEW: CAVE2 screen mask added to player controller with custom shader and render script

---
+++ CAVE2-Simulator/Scripts/Util/CAVE2ScreenMaskRenderer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CAVE2ScreenMaskRenderer : MonoBehaviour {
+
+    public enum RenderMode { None, Background, Overlay }
+    public RenderMode renderMode = RenderMode.Background;
+
+	// Update is called once per frame
+	void Update () {
+	    
+        switch(renderMode)
+        {
+            case (RenderMode.Background): GetComponent<Renderer>().material.SetFloat("_ZTest", 2); break;
+            case (RenderMode.None): GetComponent<Renderer>().material.SetFloat("_ZTest", 1); break;
+            case (RenderMode.Overlay): GetComponent<Renderer>().material.SetFloat("_ZTest", 0); break;
+        }
+	}
+}
