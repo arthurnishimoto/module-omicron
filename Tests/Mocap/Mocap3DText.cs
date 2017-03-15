a3b555ed773003b3e53cba17d66a26263bb9f7fe
Arthur Nishimoto <arthur.nishimoto@gmail.com>
2017-03-15 15:36:22 -0500

    NEW: Multi-tracked mocap object test

---
+++ Tests/Mocap/Mocap3DText.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mocap3DText : MonoBehaviour {
+
+    OmicronMocapObject mocapObject;
+
+	// Use this for initialization
+	void Start () {
+        mocapObject = GetComponentInParent<OmicronMocapObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(mocapObject)
+        {
+            GetComponent<TextMesh>().text = mocapObject.sourceID.ToString();
+            transform.LookAt(Camera.main.transform.position);
+            transform.Rotate(Vector3.up, 180);
+        }
+	}
+}
