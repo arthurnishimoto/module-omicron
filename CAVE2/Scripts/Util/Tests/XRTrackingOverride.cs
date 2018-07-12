8cc8dfb49e340751fe6042eb6da12b816f44441d
Arthur Nishimoto <arthur.nishimoto@gmail.com>
2018-07-11 22:02:40 -0500

    NEW: XR Tracking Override (Position only)

---
+++ CAVE2/Scripts/Util/Tests/XRTrackingOverride.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XRTrackingOverride : MonoBehaviour {
+
+    [SerializeField]
+    bool disablePositionalTracking;
+
+	// Use this for initialization
+	void Start () {
+        UnityEngine.XR.InputTracking.disablePositionalTracking = disablePositionalTracking;
+	}
+}
