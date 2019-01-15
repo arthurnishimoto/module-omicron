6894bcd03ebd681b498dda7ff001b0fba043dca3
Arthur Nishimoto <arthur.nishimoto@gmail.com>
2019-01-15 11:24:03 -0600

    NEW: Kinect depth streaming (slow, but functional)

--- Scripts/Util/Kinect/OmicronKinectImageClient.cs
+++ Scripts/Util/Kinect/OmicronKinectImageClient.cs
@@ -8,6 +8,9 @@ using System;
 
 public class OmicronKinectImageClient : OmicronEventClient
 {
+    public enum ImageType { Color, Depth};
+    public ImageType imageType = ImageType.Color;
+
     public int sourceID = -1; // -1 for any
 
     public int lastEventFlag;
@@ -16,7 +19,7 @@ public class OmicronKinectImageClient : OmicronEventClient
     public Texture2D texture;
     public Material outputMaterial;
 
-    int[] byteSample = new int[4];
+    public int[] byteSample = new int[4];
     public int x;
     public int y;
 
@@ -28,10 +31,24 @@ public class OmicronKinectImageClient : OmicronEventClient
         eventOptions = EventBase.ServiceType.ServiceTypeGeneric;
         InitOmicron();
 
-        texture = new Texture2D(1920, 1080);
+        int width = 1920;
+        int height = 1080;
+
+        if (imageType == ImageType.Color)
+        {
+            width = 1920;
+            height = 1080;
+        }
+        else if (imageType == ImageType.Depth)
+        {
+            width = 512;
+            height = 424;
+        }
+
+        texture = new Texture2D(width, height);
         outputMaterial.mainTexture = texture;
 
-        int pixelCount = 1920 * 1080;
+        int pixelCount = width * height;
         colorArray = new Color[pixelCount];
     }
 
@@ -49,23 +66,49 @@ public class OmicronKinectImageClient : OmicronEventClient
 
         int packetID = (int)e.flags; // 0 to 8640
 
-        y = packetID / 8; // 9 segments of 240 pixels per line
-        x = (packetID % 8) * 240;
-
-        for (int i = 0; i < extraDataSize; i+= 4)
+        if (imageType == ImageType.Color)
         {
-            int pixelID = i * (extraDataSize / 4);
-            Array.Copy(bitmapBytes, i, byteSample, 0, 4);
+            y = packetID / 8;
+            x = (packetID % 8) * 240; // 8 segments of 240 pixels per line
 
-            Color color = new Color(byteSample[2] / 255.0f, byteSample[1] / 255.0f, byteSample[0] / 255.0f);
+            for (int i = 0; i < extraDataSize; i += 4)
+            {
+                int pixelID = i * (extraDataSize / 4);
+                Array.Copy(bitmapBytes, i, byteSample, 0, 4);
 
-            //int overallImageID = packetID * (extraDataSize / 4) + pixelID;
-            //Debug.Log(overallImageID);
-            //colorArray[overallImageID] = color;
-            
-            //Debug.Log(byteSample[0] + " " + byteSample[1] + " " + byteSample[2] + " " + byteSample[3]);
-            texture.SetPixel(x, y, color);
-            x++;
+                Color color = new Color(byteSample[2] / 255.0f, byteSample[1] / 255.0f, byteSample[0] / 255.0f);
+
+                //int overallImageID = packetID * (extraDataSize / 4) + pixelID;
+                //Debug.Log(overallImageID);
+                //colorArray[overallImageID] = color;
+
+                //Debug.Log(byteSample[0] + " " + byteSample[1] + " " + byteSample[2] + " " + byteSample[3]);
+                texture.SetPixel(x, y, color);
+                x++;
+            }
+        }
+        else if( imageType == ImageType.Depth)
+        {
+            y = packetID / 4;
+            x = (packetID % 4) * 128;
+
+            extraDataSize = 848;
+
+            for (int i = 0; i < extraDataSize; i += 4)
+            {
+                int pixelID = i * (extraDataSize / 4);
+                Array.Copy(bitmapBytes, i, byteSample, 0, 4);
+
+                Color color = new Color(byteSample[2] / 255.0f, byteSample[1] / 255.0f, byteSample[0] / 255.0f);
+
+                //int overallImageID = packetID * (extraDataSize / 4) + pixelID;
+                //Debug.Log(overallImageID);
+                //colorArray[overallImageID] = color;
+
+                //Debug.Log(byteSample[0] + " " + byteSample[1] + " " + byteSample[2] + " " + byteSample[3]);
+                texture.SetPixel(x, y, color);
+                x++;
+            }
         }
         
     }
