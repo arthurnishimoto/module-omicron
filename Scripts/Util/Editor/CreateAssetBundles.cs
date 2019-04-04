3d1fd0e4034a18d5c57b4c0f660c05eddd03495f
Arthur Nishimoto <arthur.nishimoto@gmail.com>
2019-04-04 00:44:16 -0500

    NEW: Added CreateAssetBundles.cs

---
+++ Scripts/Util/Editor/CreateAssetBundles.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+
+    [MenuItem("Assets/Build AssetBundles (Uncompressed)")]
+    static void BuildAllAssetBundlesUncompressed()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.UncompressedAssetBundle, BuildTarget.StandaloneWindows);
+    }
+}
