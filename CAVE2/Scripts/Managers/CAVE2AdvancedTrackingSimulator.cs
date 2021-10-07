839d5f442b7215269ec34df27eb7fcff84e667ae
Arthur Nishimoto <arthur.nishimoto@gmail.com>
2021-10-06 23:35:42 -0500

    FIX: Unity 2020.3.7

--- CAVE2/Scripts/Managers/CAVE2AdvancedTrackingSimulator.cs
+++ CAVE2/Scripts/Managers/CAVE2AdvancedTrackingSimulator.cs
@@ -26,18 +26,27 @@
  *************************************************************************************************/
  
 using UnityEngine;
-using UnityEngine.VR;
 using System.Collections;
 
 public class CAVE2AdvancedTrackingSimulator : MonoBehaviour {
 
     public int headID = 1;
-    public float headMovementSpeed = 1;
-    public float headRotationSpeed = 25;
+
     public int wandID = 1;
 
     public bool wandUsesHeadPosition = false;
     public Vector3 wandDefaultPositionOffset = new Vector3(0.15f, 1.5f, 0.4f);
+
+    public enum TrackingSimulatorMode { Head, Wand };
+
+    [SerializeField]
+    TrackingSimulatorMode IJKLHM_mode = TrackingSimulatorMode.Head;
+
+    [SerializeField]
+    float IJKLHM_MovementSpeed = 1;
+
+    [SerializeField]
+    float IJKLHM_RotationSpeed = 25;
 
     public KeyCode toggleMouseTracking = KeyCode.Tab;
 
@@ -158,78 +167,75 @@ public class CAVE2AdvancedTrackingSimulator : MonoBehaviour {
         Vector3 headPosition = CAVE2.GetCAVE2Manager().simulatorHeadPosition;
         Vector3 headRotation = CAVE2.GetCAVE2Manager().simulatorHeadRotation;
 
-        // Vector3 wandPosition = CAVE2.GetCAVE2Manager().simulatorWandPosition;
+        Vector3 wandPosition = CAVE2.GetCAVE2Manager().simulatorWandPosition;
         // Vector3 wandRotation = CAVE2.GetCAVE2Manager().simulatorWandRotation;
 
         Vector3 translation = Vector3.zero;
+        Vector3 rotation = Vector3.zero;
 
         if ( Input.GetKey(KeyCode.I))
         {
-            translation.z += headMovementSpeed * Time.fixedDeltaTime;
+            translation.z += IJKLHM_MovementSpeed * Time.fixedDeltaTime;
         }
         if (Input.GetKey(KeyCode.K))
         {
-            translation.z -= headMovementSpeed * Time.fixedDeltaTime;
+            translation.z -= IJKLHM_MovementSpeed * Time.fixedDeltaTime;
         }
         if (Input.GetKey(KeyCode.J))
         {
-            translation.x -= headMovementSpeed * Time.fixedDeltaTime;
+            translation.x -= IJKLHM_MovementSpeed * Time.fixedDeltaTime;
         }
         if (Input.GetKey(KeyCode.L))
         {
-            translation.x += headMovementSpeed * Time.fixedDeltaTime;
+            translation.x += IJKLHM_MovementSpeed * Time.fixedDeltaTime;
         }
 
         if (Input.GetKey(KeyCode.H))
         {
-            translation.y += headMovementSpeed * Time.fixedDeltaTime;
+            translation.y += IJKLHM_MovementSpeed * Time.fixedDeltaTime;
         }
         if (Input.GetKey(KeyCode.N))
         {
             if(headPosition.y > 0.1f)
-                translation.y -= headMovementSpeed * Time.fixedDeltaTime;
+                translation.y -= IJKLHM_MovementSpeed * Time.fixedDeltaTime;
         }
 
         if (Input.GetKey(KeyCode.U))
         {
-            headRotation.y -= headRotationSpeed * Time.fixedDeltaTime;
+            rotation.y -= IJKLHM_RotationSpeed * Time.fixedDeltaTime;
         }
         if (Input.GetKey(KeyCode.O))
         {
-            headRotation.y += headRotationSpeed * Time.fixedDeltaTime;
+            rotation.y += IJKLHM_RotationSpeed * Time.fixedDeltaTime;
         }
 
-        headPosition.z += translation.z * Mathf.Cos(Mathf.Deg2Rad * headRotation.y);
-        headPosition.x += translation.z * Mathf.Sin(Mathf.Deg2Rad * headRotation.y);
-
-        headPosition.z += translation.x * Mathf.Cos(Mathf.Deg2Rad * (90 + headRotation.y));
-        headPosition.x += translation.x * Mathf.Sin(Mathf.Deg2Rad * (90 + headRotation.y));
-
-        headPosition.y += translation.y;
-
-        CAVE2.GetCAVE2Manager().simulatorHeadPosition = headPosition;
-        CAVE2.GetCAVE2Manager().simulatorHeadRotation = headRotation;
-
-        /*
-        if (CAVE2.GetHeadObject(headID) != null && CAVE2.GetWandObject(wandID) != null)
+        if (IJKLHM_mode == TrackingSimulatorMode.Head)
         {
-            Transform wandObject = CAVE2.GetWandObject(wandID).transform;
-            Transform headObject = CAVE2.GetHeadObject(headID).transform;
-            Transform playerController = CAVE2.GetWandObject(wandID).transform.root;
+            headPosition.z += translation.z * Mathf.Cos(Mathf.Deg2Rad * headRotation.y);
+            headPosition.x += translation.z * Mathf.Sin(Mathf.Deg2Rad * headRotation.y);
 
-            if (wandObject.transform.parent != headObject)
-                wandObject.transform.parent = headObject;
+            headPosition.z += translation.x * Mathf.Cos(Mathf.Deg2Rad * (90 + headRotation.y));
+            headPosition.x += translation.x * Mathf.Sin(Mathf.Deg2Rad * (90 + headRotation.y));
 
-            if (mouseWandOffsetTriggered)
-            {
-                CAVE2.GetCAVE2Manager().simulatorWandPosition = wandPosition + mouseTrackingOffset;
-            }
-            else
-            {
-                wandPosition.y = wandDefaultPositionOffset.y - headPosition.y;
-                CAVE2.GetCAVE2Manager().simulatorWandPosition = wandPosition;
-            }
+            headPosition.y += translation.y;
+
+            headRotation.y += rotation.y;
+
+            CAVE2.GetCAVE2Manager().simulatorHeadPosition = headPosition;
+            CAVE2.GetCAVE2Manager().simulatorHeadRotation = headRotation;
         }
-        */
+        else if (IJKLHM_mode == TrackingSimulatorMode.Wand)
+        {
+            wandPosition.z += translation.z * Mathf.Cos(Mathf.Deg2Rad * headRotation.y);
+            wandPosition.x += translation.z * Mathf.Sin(Mathf.Deg2Rad * headRotation.y);
+
+            wandPosition.z += translation.x * Mathf.Cos(Mathf.Deg2Rad * (90 + headRotation.y));
+            wandPosition.x += translation.x * Mathf.Sin(Mathf.Deg2Rad * (90 + headRotation.y));
+
+            wandPosition.y += translation.y;
+
+            wandDefaultPositionOffset = wandPosition;
+        }
+
     }
 }
