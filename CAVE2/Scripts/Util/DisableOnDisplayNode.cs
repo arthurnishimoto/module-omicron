41c46704b4ea1e034ba52c97b577c9313775e433
Arthur Nishimoto <arthur.nishimoto@gmail.com>
2022-03-03 02:01:23 -0600

    NEW: Simple disable on display node script

---
+++ CAVE2/Scripts/Util/DisableOnDisplayNode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOnDisplayNode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!CAVE2.IsMaster())
+        {
+            gameObject.SetActive(false);
+        }    
+    }
+}
