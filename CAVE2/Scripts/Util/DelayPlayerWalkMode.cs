ec2c078a71d08f54408883f67f9a41b8964390d0
Arthur Nishimoto <arthur.nishimoto@gmail.com>
2022-03-05 01:52:58 -0600

    NEW: Added DelayPlayerWalkMode script to enable walk mode after physics settles after a few frames

---
+++ CAVE2/Scripts/Util/DelayPlayerWalkMode.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Simple script to toggle CAVE2PlayerController from Drive mode to Walk mode
+// after a time delay. This is useful in physics simulations where the world
+// needs to 'settle' on startup.
+
+public class DelayPlayerWalkMode : MonoBehaviour
+{
+    [SerializeField]
+    float delay = 5;
+
+    float timer;
+
+    [SerializeField]
+    bool done;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer < delay)
+        {
+            timer += Time.deltaTime;
+        }
+        else if(!done)
+        {
+            GetComponent<CAVE2WandNavigator>().SetNavModeWalk(true);
+            done = true;
+        }
+    }
+}
